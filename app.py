be9e7d1f593eed295088c13deed96456b167e5e5
Pedro Daniel <p57640487@gmail.com>
2025-12-18 11:06:20 -0300

    .

--- app.py
+++ app.py
@@ -58,11 +58,11 @@ def sync_to_gdrive(local_folders):
         print(f">> Destino: {remote_path}")
 
         try:
-            # Usamos o sync para garantir que o que está no local seja igual ao remoto
+            #  o sync para garantir que o que está no local seja igual ao remoto
             rclone.sync(
                 local_path,
                 remote_path,
-                flags=['--progress', '--drive-acknowledge-abuse']
+                flags=['--progress', '--drive-acknowledge-abuse', '--transfers=8', '--drive-chunk-size=64M', '--tpslimit=5']
             )
             print(f"SUCESSO: {supermarket_name} atualizado.")
         except Exception as e:
